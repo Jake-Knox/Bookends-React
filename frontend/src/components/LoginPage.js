488d1db20dd9b3610684ca8bbb785fd969d3faec
jake-knox- <jakeknox11@hotmail.co.uk>
2024-01-30 15:48:32 +0000

    switch to not use mongoose after shcema issues, working connection, VERY basic login. ready to move on

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -8,6 +8,7 @@ axios.defaults.baseURL = 'http://localhost:5000';
 
 function LoginPage() {
     const [message, setMessage] = useState('');
+    const [message2, setMessage2] = useState('');
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -17,6 +18,16 @@ function LoginPage() {
             const response = await axios.post('/api/test');
             console.log(response);
             setMessage(response.data.message);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const fetchBookData = async () => {
+        try {
+            const response = await axios.post('/api/books');
+            console.log(response);
+            setMessage2(response.data.length);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -38,8 +49,11 @@ function LoginPage() {
     return (
         <div>
             <h2>Login Page</h2>
-            <button onClick={fetchData}>Fetch Data</button>
+            <button onClick={fetchData}>Fetch Test Data</button>
             <p>{message}</p>
+
+            <button onClick={fetchBookData}>Fetch Book Data</button>
+            <p>{message2}</p>
             {/* Add login form here */}
             <form onSubmit={handleSubmit}>
                 <div>
--- server/server.js
+++ server/server.js
@@ -1,41 +1,112 @@
 // require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const config = require('./config');
 
-// Import the User model for mongoose
-const User = require('./userModel'); // Adjust the path as needed
-// Using another 
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = config.MONGODB_URI;
+const uri = config.MONGODB_URI;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors()); // Allow Cross-Origin requests
 
-// MongoDB connection
-mongoose.connect(MONGODB_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
-mongoose.set('debug', true);
-
-// Test endpoint
-app.post('/api/test', async (req, res) => {
-    console.log("test request");
-    try {
-        res.status(201).json({ message: 'Hello World' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error registering user' });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
     }
 });
+
+
+
+// Connect to the MongoDB server
+client.connect()
+    .then(() => {
+
+        // all of this code is kept inside the db connection so that the site doesn't have to reconnect. Inefficient?
+        console.log('Connected to MongoDB');
+        const db = client.db('bookshelf-db');
+
+        // Set up database route handlers
+        // Test endpoint
+        app.post('/api/test', async (req, res) => {
+            console.log("test request");
+            try {
+                res.status(201).json({ message: 'Hello World' });
+            } catch (error) {
+                res.status(500).json({ error: 'Error registering user' });
+            }
+        });
+
+
+        app.post('/api/books', (req, res) => {
+            // console.log("books request");
+            const collection = db.collection('books');
+            collection.find().toArray((err, books) => {
+                if (err) {
+                    console.error('Error retrieving books:', err);
+                    res.sendStatus(500);
+                } else {
+                    res.json(books);
+                }
+            });
+        });
+
+
+        // Login endpoint
+        app.post('/api/login', async (req, res) => {
+            console.log("login request");
+
+            const { username, password } = req.body;
+            console.log(`${username}, ${password}`);
+
+            db.collection('users').findOne({ username }, (err, user) => {
+                if (err) {
+                    console.error('Error finding user:', err);
+                    res.sendStatus(500);
+                } else if (!user) {
+                    res.status(401).json({ message: 'User not found' });
+                } else {
+                    // Compare the provided password with the hashed password stored in the user object
+                    bcrypt.compare(password, user.password, (err, result) => {
+                        if (err) {
+                            console.error('Error comparing passwords:', err);
+                            res.sendStatus(500);
+                        } else if (result) {
+                            // Passwords match, authentication successful
+                            // req.session.isLoggedIn = true;
+                            // req.session.username = username;
+                            // console.log('isLoggedIn:', req.session.isLoggedIn);
+                            // console.log('username:', req.session.username);
+                            res.status(200).json({ message: 'Authentication successful' });
+                            // res.redirect('/profile'); // redirected from login.js       
+                        } else {
+                            // Passwords do not match, authentication failed
+                            res.status(401).json({ message: 'Invalid username or password' });
+                        }
+                    });
+                }
+            });
+        });
+
+
+
+
+
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+
+
+
 
 // Register endpoint
 app.post('/api/register', async (req, res) => {
@@ -58,49 +129,7 @@ app.post('/api/register', async (req, res) => {
     }
 });
 
-// Login endpoint
-app.post('/api/login', async (req, res) => {
-    console.log("login request");
-    try {
 
-        // testing
-
-        // Query all documents from the 'users' collection
-        User.find({})
-            .then(users => {
-                // Log the fetched documents
-                console.log('Users in the collection:', users);
-            })
-            .catch(error => {
-                // Handle any errors that occur during the query
-                console.error('Error querying users collection:', error);
-            });
-
-
-
-        const { username, password } = req.body;
-        console.log(`${username}, ${password}`);
-
-        const jane = "janeAusten1";
-        const user = await User.findOne({ username: jane });
-        console.log(user);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
-
-        const validPassword = await bcrypt.compare(password, user.password);
-
-        if (!validPassword) {
-            return res.status(401).json({ error: 'Invalid password' });
-        }
-
-        // Generate and return JWT token here
-
-        res.json({ message: 'Login successful' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error logging in' });
-    }
-});
 
 // Change password endpoint
 app.put('/api/change-password', async (req, res) => {
